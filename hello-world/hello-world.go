a56a6f59bf4389e7b6022e2fe636e866d76cdfbe
superdbop <superdbop@gmail.com>
2017-05-08 21:57:44 -0500

    Fixed typos. Formatted with go fmt

--- hello-world/hello-world.go
+++ hello-world/hello-world.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/superdbop/learning-go/stringutil"
+)
 
 func main() {
-    fmt.Printf("hello, world!\n")
+	fmt.Printf("hello, world!\n")
+	fmt.Printf(stringutil.Reverse("hello, world!") + "\n")
 }
--- stringutil/reverse.go
+++ stringutil/reverse.go
@@ -1,13 +1,13 @@
 // Package stringutil contains utility functions for working with strings.
 package stringutil
 
-// Reverse returns its argument string revered run-wise left to right.
+// Reverse returns its argument string reversed rune-wise left to right.
 func Reverse(s string) string {
-    r := []rune(s)
+	r := []rune(s)
 
-    for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
-        r[i], r[j] = r[j], r[i]
-    }
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
 
-    return string(r)
+	return string(r)
 }
